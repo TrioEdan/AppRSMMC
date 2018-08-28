81322ada20e18e26a0400d7f656897df2fa646c7
akbarpambudiutomo <akbarpambudi31@gmail.com>
2018-08-28 11:35:21 +0700

    commit saya

--- app/src/main/java/com/example/akbar/rumahsakitmusimedikacendikia/ServerAPI.java
+++ app/src/main/java/com/example/akbar/rumahsakitmusimedikacendikia/ServerAPI.java
@@ -1,10 +1,10 @@
 package com.example.akbar.rumahsakitmusimedikacendikia;
 
 public class ServerAPI {
+    //ip address diganti dengan ip address komputer yg terhubung ke jaringan
     public static final String URL_ROOT = "http://192.168.43.105/crud_project/Api.php?apicall=";
 
     public static final String URL_CREATE = URL_ROOT + "createdata";
     public static final String URL_READ = URL_ROOT + "getdata";
-    public static final String URL_UPDATE = URL_ROOT + "updatedata";
-    public static final String URL_DELETE = URL_ROOT + "deletedata&id=";
+
 }
